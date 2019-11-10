5a6ba5f6dd802688adfd05fa585aafef4f13799f
Conner Davis <connergdavis@gmail.com>
2019-11-09 22:16:33 -0800

    added primary color picker, changed layout to three rows

--- src/visualize.js
+++ src/visualize.js
@@ -1,96 +1,110 @@
-const audio     = document.getElementById('vis-audio');
-const container = document.getElementById('vis-container');
-const canvas    = document.getElementById('vis-canvas');
-const file      = document.getElementById('vis-file');
-const name      = document.getElementById('vis-name');
+const audio     = document.getElementById('audio');
+const canvas    = document.getElementById('canvas');
+const file      = document.getElementById('file');
+const name      = document.getElementById('name');
 
-const drawType = document.getElementById('vis-drawtype');
+const drawType = document.getElementById('draw-type');
+const color = document.getElementById('color');
 
-function freqColor( x )
+const scaling = 256.0;
+
+function hexToRgb( )
 {
-    let r, g, b;
-    
-    r = 128;
-    g = 0;
-    b = 255;
+    const num = parseInt(color.value.substring(1), 16);
 
-    if (x > 100)
-        r = x;
+    const r = (num >> 16) & 255;
+    const g = (num >> 8) & 255;
+    const b = num & 255;
 
-    if (x > 200)
-        g = x * 0.50;
-    else if (x > 150)
-        g = x * 0.05;
-    
-    return `rgb(${r},${g},${b})`;
+    return {
+        r: r,
+        g: g,
+        b: b
+    };
 }
 
-function drawFreqWave( ctx, freqs, WIDTH, HEIGHT )
+function uint8ToRgb( x )
 {
-    const max = Math.max.apply(Math, freqs);
-    let y = HEIGHT;
+    // base color: "primary" purple
+    let rgb = hexToRgb();
+
+    // if signal is strong enough, vary color to be lighter
+    if (x > 96)
+    {
+        rgb.r = rgb.r + (255 - x) * 0.50;
+        rgb.g = rgb.g + (255 - x) * 0.50;
+        rgb.b = rgb.b + (255 - x) * 0.50;
+    }
+
+    return `rgb(${rgb.r},${rgb.g},${rgb.b})`;
+}
+
+function drawWave( ctx, freqs, max, width, height )
+{
+    let y = height;
 
     ctx.lineWidth = 2;
-    for (let x = 0; x < WIDTH; ++x)
+    
+    for (let x = 0; x < width; ++x)
     {
-        ctx.strokeStyle = freqColor(freqs[x]);
-        ctx.beginPath();
+        ctx.strokeStyle = uint8ToRgb(freqs[x]);
 
+        ctx.beginPath();
         ctx.moveTo(x, y);
-        y = HEIGHT - freqs[x] * (HEIGHT / 256.0);
+
+        // update y after each stroke in order to finish line
+        y = height - freqs[x] * (height / scaling);
         ctx.lineTo(x, y);
 
         ctx.stroke();
     }
 }
 
-function drawFreqBars( ctx, freqs, WIDTH, HEIGHT )
+function drawBars( ctx, freqs, max, width, height )
 {
-    const max = Math.max.apply(Math, freqs);
     let barHeight;
 
-    for (let x = 0; x < WIDTH; x += 10)
+    for (let x = 0; x < width; x += 10)
     {
-        barHeight = freqs[x] * (HEIGHT / 256.0);
+        barHeight = freqs[x] * (height / 180.0);
 
-        ctx.fillStyle = freqColor(freqs[x]);
-        ctx.fillRect(x, HEIGHT - barHeight, 10, barHeight);
+        ctx.fillStyle = uint8ToRgb(freqs[x]);
+        ctx.fillRect(x, height - barHeight, 10, barHeight);
     }
 }
 
-function drawTimeOsc( ctx, time, WIDTH, HEIGHT )
+function drawOsc( ctx, time, max, width, height )
 {
     let risingEdge = 0;
     let edgeThreshold = 5;
 
     ctx.lineWidth = 2;
-    ctx.strokeStyle = 'rgb(128,0,255)';
+    const rgb = hexToRgb(color.value);
+    ctx.strokeStyle = `rgb(${rgb.r},${rgb.g},${rgb.b})`;
     ctx.beginPath();
 
-    while (time[risingEdge++] - 128 > 0 && risingEdge <= WIDTH);
-    if (risingEdge >= WIDTH) risingEdge = 0;
+    while (time[risingEdge++] - 128 > 0 && risingEdge <= width);
+    if (risingEdge >= width) risingEdge = 0;
 
-    while (time[risingEdge++] - 128 < edgeThreshold && risingEdge <= WIDTH);
-    if (risingEdge >= WIDTH) risingEdge = 0;
+    while (time[risingEdge++] - 128 < edgeThreshold && risingEdge <= width);
+    if (risingEdge >= width) risingEdge = 0;
 
-    for (let x = risingEdge; x < time.length && x - risingEdge < WIDTH; ++x)
+    for (let x = risingEdge; x < time.length && x - risingEdge < width; ++x)
     {
-        ctx.lineTo(x - risingEdge, HEIGHT - time[x] * (HEIGHT / 256.0));
+        ctx.lineTo(x - risingEdge, height - time[x] * (height / scaling));
     }
     ctx.stroke();
 }
 
-function visualize()
+function visualize( )
 {
-    file.addEventListener('change', function ()
+    file.addEventListener('change', function ( )
     {
         // get file uploaded by user
         let files = this.files;
         audio.src = URL.createObjectURL(files[0]);
 
-        // set file name heading
-        name.textContent = `Playing => ${files[0].name}`;
-
+        // create HTML 5 canvas contexts to draw / analyse audio
         const ctx = canvas.getContext('2d');
         const ctxAudio = new (window.AudioContext || window.webkitAudioContext)();
 
@@ -103,31 +117,42 @@ function visualize()
         src.connect(analyser);
         analyser.connect(ctxAudio.destination);
 
+        // set file name heading
+        document.getElementById('now-playing').textContent = 'Now playing';
+        name.textContent = `${files[0].name}`;
+
         function renderFrame()
         {
-            const WIDTH = canvas.width = canvas.getBoundingClientRect().width;
-            const HEIGHT = canvas.height = canvas.getBoundingClientRect().height;
+            const width = canvas.width = canvas.getBoundingClientRect().width;
+            const height = canvas.height = canvas.getBoundingClientRect().height;
 
             ctx.fillStyle = 'rgb(22, 22, 22)';
-            ctx.fillRect(0, 0, WIDTH, HEIGHT);
+            ctx.fillRect(0, 0, width, height);
 
-            const draw = drawType.options[drawType.selectedIndex].text;
-            if (draw === 'Oscilloscope')
+            const type = drawType.options[drawType.selectedIndex].text;
+            if (type === 'Oscilloscope')
             {
                 const time = new Uint8Array(analyser.frequencyBinCount);
                 analyser.getByteTimeDomainData(time);
 
-                drawTimeOsc(ctx, time, WIDTH, HEIGHT);
+                const max = Math.max.apply(Math, time);
+                drawOsc(ctx, time, max, width, height);
             }
             else
             {
                 const freqs = new Uint8Array(analyser.frequencyBinCount);
                 analyser.getByteFrequencyData(freqs);
 
-                if (draw === 'Bar')
-                    drawFreqBars(ctx, freqs, WIDTH, HEIGHT);
-                else if (draw === 'Wave')
-                    drawFreqWave(ctx, freqs, WIDTH, HEIGHT);
+                const max = Math.max.apply(Math, freqs);
+
+                if (type === 'Bar')
+                {
+                    drawBars(ctx, freqs, max, width, height);
+                }
+                else if (type === 'Wave')
+                {
+                    drawWave(ctx, freqs, max, width, height);
+                }
             }
 
             requestAnimationFrame(renderFrame);
@@ -139,7 +164,7 @@ function visualize()
     });
 }
 
-function record()
+function record( )
 {
     let mediaRecorder;
     let recordedBlobs;
@@ -225,8 +250,8 @@ function record()
         mediaRecorder.start(100); // collect 100ms of data
         console.log('MediaRecorder started', mediaRecorder);
 
-        document.getElementById('vis-recording').textContent = 'Recording';
-        document.getElementById('vis-recording').classList.add('recording');
+        document.getElementById('recording').textContent = 'Recording';
+        document.getElementById('recording').classList.add('recording');
     }
 
     function stopRecording() {
@@ -234,8 +259,8 @@ function record()
         console.log('Recorded Blobs: ', recordedBlobs);
         video.controls = true;
 
-        document.getElementById('vis-recording').textContent = 'Not Recording..';
-        document.getElementById('vis-recording').classList.remove('recording');
+        document.getElementById('recording').textContent = 'Not Recording..';
+        document.getElementById('recording').classList.remove('recording');
     }
 
     function play() {
@@ -258,6 +283,6 @@ function record()
     }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function ( ) {
     visualize();
 });
